62fabfea97164d43640fba512adafb1ff2d38477
Alejandro ASTUDILLO VIGOYA <alejandro.astudillovigoya@kuleuven.be>
2022-02-14 09:57:29 +0100

    create Python package for Robot models

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# robot-models-meco
+# Robot-models-meco
 
+A repository of robot models built with Pinocchio and CasADi.
+
+## Available robots
+| Robot | Degrees of freedom | Alias |
+|-------|-------------|-------|
+| Kinova Gen3      |     7   |  `kinova`     |
+
+
+
+For every robot in the table, this repository includes functions for:
+- Forward dynamics
+- Inverse dynamics
+- Forward kinematics
+- Analytical derivatives of forward dynamics
+- Analytical derivatives of inverse dynamics
---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+NAME = "robotsMECO"
+VERSION = "0.0.1"
+AUTHORS = "Alejandro Astudillo, Ajay Sathya"
+MAINTAINER_EMAIL = "alejandro.astudillovigoya@kuleuven.be"
+DESCRIPTION = "A repository of robot models built with Pinocchio and CasADi"
+KEYWORDS = "robot models pinocchio casadi"
+URL = "https://gitlab.kuleuven.be/meco-software/robot-models-meco"
+LICENSE = 'GNU LGPLv3'
+
+setuptools.setup(
+    name=NAME,
+    version=VERSION,
+    author=AUTHORS,
+    author_email=MAINTAINER_EMAIL,
+    description=DESCRIPTION,
+    keywords=KEYWORDS,
+    license=LICENSE,
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url=URL,
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'casadi>=3.4,<4.0',
+    ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering"
+    ],
+    python_requires='>=3.6',
+)
