3e85b67db03d151da8a2d05bf2eb79632b9d5c0b
SHKim-HYU <tjsghd101@naver.com>
2025-04-03 13:21:14 +0900

    yl012 update

---
+++ robotshyu/robots/HYUSPA/hyu_spa_description/loadurdf.py
@@ -0,0 +1,8 @@
+import pybullet as p
+import pybullet_data
+p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+planeId = p.loadURDF("plane.urdf")
+robot = p.loadURDF("hyu_spa.urdf")
+
